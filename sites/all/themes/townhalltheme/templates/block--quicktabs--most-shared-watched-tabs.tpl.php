320d7ce624a50ffd5eec1a5b3a4b793d87365ed2
Andrei Sapeshko <andrei.sapeshko@gmail.com>
2013-09-13 12:35:58 +0300

    Add '.idea' forlder into '.gitignore'

--- sites/all/themes/townhalltheme/templates/block--quicktabs--most-shared-watched-tabs.tpl.php
+++
@@ -1,9 +0,0 @@
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
-  <?php print render($title_prefix); ?>
-  <?php print render($title_suffix); ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php print $content ?>
-  </div>
-</div>
