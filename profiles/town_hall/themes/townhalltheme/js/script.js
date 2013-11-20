96030745b388ade5d7e29a35305fd8a16424232f
Uladzimir Karol <vladimir.korol@warecorp.com>
2013-11-20 15:22:23 +0000

    update

--- profiles/town_hall/themes/townhalltheme/js/script.js
+++ profiles/town_hall/themes/townhalltheme/js/script.js
@@ -5,7 +5,36 @@ jQuery("#scroll-top").click(function() {
   jQuery("html, body").animate({ scrollTop: 0 }, "fast");
   return false;
  });
-
+visibility1=false;
+visibility2=false;
+jQuery("#block-th-main-menu-th-main-menu #edit_jump_chosen a.chosen-single").click(function() {  
+  if (visibility1==false)
+	{
+		jQuery("#block-th-main-menu-th-main-menu #edit_jump__2_chosen .chosen-drop").css({"display": "none"});
+  jQuery("#block-th-main-menu-th-main-menu #edit_jump_chosen .chosen-drop").css({"display": "block"});
+  visibility1=true;
+  visibility2=false;
+	}
+	else {
+		 jQuery("#block-th-main-menu-th-main-menu #edit_jump_chosen .chosen-drop").css({"display": "none"});
+		 visibility1=false;
+	}
+  return false;
+ });
+jQuery("#block-th-main-menu-th-main-menu #edit_jump__2_chosen a.chosen-single").click(function() {
+if (visibility2==false)
+	{
+  jQuery("#block-th-main-menu-th-main-menu #edit_jump_chosen .chosen-drop").css({"display": "none"});
+  jQuery("#block-th-main-menu-th-main-menu #edit_jump__2_chosen .chosen-drop").css({"display": "block"});
+  visibility2=true;
+  visibility1=false;
+	}
+	else {
+		 jQuery("#block-th-main-menu-th-main-menu #edit_jump__2_chosen .chosen-drop").css({"display": "none"});
+		 visibility2=false;
+	}
+  return false;
+ });
 	var text_h = jQuery(".node-candidate .field-type-text-with-summary").height();
 	var img_h = jQuery(".node-candidate .field-name-field-image img").height();
 	if (text_h > img_h)
