7541a000b0268c19e55fec8e5be7eadbad7696b3
Uladzimir Karol <vladimir.korol@warecorp.com>
2013-11-25 14:16:03 +0000

    update

--- profiles/town_hall/themes/townhalltheme/js/script.js
+++ profiles/town_hall/themes/townhalltheme/js/script.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-jQuery('.view-id-candidates #edit-jump').chosen({disable_search:true});
+jQuery('.ctools-jump-menu-select').chosen({disable_search:true});
 jQuery('.view-id-parties #edit-jump--2').chosen({disable_search:true});
 jQuery("#scroll-top").click(function() {
   jQuery("html, body").animate({ scrollTop: 0 }, "fast");
@@ -35,16 +35,5 @@ if (visibility2==false)
 	}
   return false;
  });
-	var text_h = jQuery(".node-candidate .field-type-text-with-summary").height();
-	var img_h = jQuery(".node-candidate .field-name-field-image img").height();
-	if (text_h > img_h)
-	{
-		jQuery(".node-candidate .field-name-field-image").css({"height": text_h + 100});
-	}
-	var text_h2 = jQuery(".node-party .field-type-text-with-summary").height() + jQuery(".node-party .field-name-field-th-video").height();
-	var img_h2 = jQuery(".node-party .field-name-field-image img").height();
-	if (text_h2 > img_h2)
-	{
-	jQuery(".node-party .field-name-field-image").css({"height": text_h2 + 100});
-	}
 });
+
